3b69c035deecabff5be999a31811df13a23b14f6
LisaVysochyn <90222844+LisaVysochyn@users.noreply.github.com>
2021-10-28 02:02:48 +0300

    Update Q5.py
    добавляй опис та приклади, дякую

--- Q5.py
+++ Q5.py
@@ -1,9 +1,15 @@
+"""Write a program that asks the user to enter integer number. Then check if the number is prime.
+>>>Введіть число: 3
+Число є простим
+>>>Введіть число: 4
+Число не є простим
+"""
 a = int(input("Введіть число: "))
 k = 0
 for i in range(2, a // 2+1):
     if (a % i == 0):
         k = k + 1
 if (k <= 0):
-    print("Число є простим: ")
+    print("Число є простим")
 else:
-    print("Число не є простим: ")
+    print("Число не є простим")
